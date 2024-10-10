0c7ca6ded7ba88d202d30d9f6cb0c873b30983c8
tongene <ngene.christina@gmail.com>
2024-10-10 16:51:08 -0100

    deploy-3.3-10/10

--- components/TabNav.jsx
+++ components/TabNav.jsx
@@ -4,7 +4,7 @@ import ChangingText from "./Changing"
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown, faCaretLeft, faCaretUp } from "@fortawesome/free-solid-svg-icons"; 
-// import { newsViews, sideBarNewsItems, sidePanelNewsItems } from "@/app/news/rootpostsHandle";
+import { newsViews, sideBarNewsItems, sidePanelNewsItems } from "@/app/news/rootpostsHandle";
 import { useEffect, useState } from "react";
 
 const TabNav = ({trends}) => {
